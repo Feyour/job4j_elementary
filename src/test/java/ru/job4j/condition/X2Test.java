e28798322b213542f61a388cf540c1f71b819c22
feyour <feyour@gmail.com>
2022-03-24 19:23:10 +0300

    1.2. Входные данные в тесте [#297750]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -11,7 +11,7 @@ public class X2Test {
         int b = 0;
         int c = 0;
         int x = 2;
-        int expected = 3;
+        int expected = 40;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
     }
