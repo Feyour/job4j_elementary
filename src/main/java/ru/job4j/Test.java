bd487b57bf00c9b781be2871926fce2a6cdce19e
feyour <feyour@gmail.com>
2022-03-24 12:39:02 +0300

    1. Модульные тесты. [#159435]

---
+++ src/main/java/ru/job4j/Test.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+public class Test {
+    public static void main(String[] args) {
+        int i = 4;
+        System.out.println(i);
+        i += 4;
+        System.out.println(i);
+        i /= 2;
+        System.out.println(i);
+        long a = (int) i;
+        System.out.println(a);
+        if (a < 3)
+        System.out.println(18);
+        else
+            System.out.println(19);
+    }
+}
