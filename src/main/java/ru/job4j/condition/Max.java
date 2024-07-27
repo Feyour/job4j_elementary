431a3fe08c90f01a65d2b900bd7b97b96d53e16b
feyour <feyour@gmail.com>
2024-07-27 13:48:52 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -5,9 +5,15 @@ public class Max {
         return left > right ? left : right;
     }
 
+    public static int max(int left, int right, int s) {
+        return Math.max(left, Math.max(right, s));
+    }
+
     public static void main(String[] args) {
         int rsl = Max.max(4, 5);
+        int rsl2 = Max.max(4, 5, 7);
         System.out.println(rsl);
+        System.out.println(rsl2);
 
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,30 +6,37 @@ import org.junit.Assert;
 public class MaxTest {
 
     @Test
-    public void whenMax3to4Then4() {
+    public void whenMax3to7Then7() {
         int left = 3;
         int right = 4;
+        int s = 7;
         int result = Max.max(left, right);
-        int expected = 4;
-        Assert.assertEquals(result, expected);
+        int result2 = Max.max(left, right, s);
+        int expected = 7;
+        Assert.assertEquals(result, result2, expected);
+
     }
 
     @Test
     public void whenMax6to2Then6() {
         int left = 6;
         int right = 2;
+        int s = 3;
         int result = Max.max(left, right);
+        int result2 = Max.max(left, right, s);
         int expected = 6;
-        Assert.assertEquals(result, expected);
+        Assert.assertEquals(result, result2, expected);
     }
 
     @Test
     public void whenMax5to5Then5() {
         int left = 5;
         int right = 5;
+        int s = 5;
         int result = Max.max(left, right);
+        int result2 = Max.max(left, right, s);
         int expected = 5;
-        Assert.assertEquals(result, expected);
+        Assert.assertEquals(result, result2, expected);
 
     }
 
