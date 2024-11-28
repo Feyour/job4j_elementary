74421cf32df91f2bdd1a7924b2a877019d6ca455
feyour <feyour@gmail.com>
2024-11-28 21:03:55 +0300

    2.0. FileOutputStream [#252488]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,5 +1,8 @@
 package ru.job4j;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Multiple {
     public static void main(String[] args) {
         System.out.println("1 * 2 = 2");
@@ -11,5 +14,25 @@ public class Multiple {
         System.out.println("1 * 8 = 8");
         System.out.println("1 * 9 = 9");
 
+        try (FileOutputStream output = new FileOutputStream("data/dataresult.txt")) {
+            output.write("1 * 2 = 2".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            output.write("1 * 3 = 3".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            output.write("1 * 4 = 4".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            output.write("1 * 5 = 5".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            output.write("1 * 6 = 6".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            output.write("1 * 7 = 7".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            output.write("1 * 8 = 8".getBytes());
+            output.write(System.lineSeparator().getBytes());
+            output.write("1 * 9 = 9".getBytes());
+            output.write(System.lineSeparator().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
